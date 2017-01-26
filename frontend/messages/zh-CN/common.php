fb616d6bdd318122f319fae36659e07a3487a455
gyhkevin <haiven1989@126.com>
2017-01-26 23:45:11 +0800

    init project and url manager

---
+++ frontend/messages/zh-CN/common.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/1/16
+ * Time: 12:28
+ */
+return [
+    'Blog' => '博客',
+    'Home' => '首页',
+    'About' => '关于',
+    'Contact' => '联系',
+    'Login' => '登陆',
+    'Signup' => '注册',
+];
